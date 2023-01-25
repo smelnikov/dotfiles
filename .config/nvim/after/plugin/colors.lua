7c2ee463924b14a08bfb47f82f60e7279d6e2078
smelnikov <smelnikov@uniqsoft.ae>
2023-01-25 21:35:17 +0400

    add transparency to SignColumn

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -4,6 +4,7 @@ function ColorThings(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "SignColumn", { fg = "none" })
 		
 end
 
