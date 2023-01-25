4facdf8b8d649e5e62742cb0e2246045e23a031a
smelnikov <smelnikov@uniqsoft.ae>
2023-01-25 13:33:37 +0400

    show hidden items in nerdtree to nvim

--- .config/nvim/after/plugin/nerdtree.lua
+++ .config/nvim/after/plugin/nerdtree.lua
@@ -1,3 +1,5 @@
+vim.g.NERDTreeShowHidden = 1
+
 vim.keymap.set('n', '<leader>n', vim.cmd.NERDTreeFocus)
 vim.keymap.set('n', '<C-t>', vim.cmd.NERDTreeToggle)
 vim.keymap.set('n', '<C-f>', vim.cmd.NERDTreeFind)
