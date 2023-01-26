ce6a19f9c7c326afd9c6717c62b579a8f7201d86
smelnikov <smelnikov@uniqsoft.ae>
2023-01-26 20:06:32 +0400

    fix typo in nerdtree.lua

--- .config/nvim/after/plugin/nerdtree.lua
+++ .config/nvim/after/plugin/nerdtree.lua
@@ -10,5 +10,6 @@ vim.keymap.set('n', '<C-t>', vim.cmd.NERDTreeToggle, { desc = 'Toggle tree' })
 vim.keymap.set(
   'n',
   '<C-f>',
-  vim.cmd.NERDTreeFind { desc = 'Find current buffer in tree' }
+  vim.cmd.NERDTreeFind,
+  { desc = 'Find current buffer in tree' }
 )
