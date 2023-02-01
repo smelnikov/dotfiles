1a821670887e82b5ebc4644654d1ea639eb48e0d
smelnikov <smelnikov@uniqsoft.ae>
2023-02-01 15:44:53 +0400

    remap for yank into plusunnamed

--- .config/nvim/lua/shi/remap.lua
+++ .config/nvim/lua/shi/remap.lua
@@ -10,3 +10,5 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
