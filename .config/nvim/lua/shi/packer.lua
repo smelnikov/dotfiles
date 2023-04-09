de4fd131f8aafaf7998e7c5cd2ea6fd4bc9acd3c
smelnikov <smelnikov@uniqsoft.ae>
2023-04-09 16:06:49 +0400

    nvim: add treesitter-context

--- .config/nvim/lua/shi/packer.lua
+++ .config/nvim/lua/shi/packer.lua
@@ -13,6 +13,7 @@ return require('packer').startup {
       'nvim-treesitter/nvim-treesitter',
       run = ':TSUpdate',
     }
+    use 'nvim-treesitter/nvim-treesitter-context'
     use 'mbbill/undotree'
     use 'tpope/vim-fugitive'
     use 'tpope/vim-surround'
