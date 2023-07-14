2d25191cc81f0ac44fcb068e212d264362b64f60
Sergei Melnikov <smelnikov@uniqsoft.ae>
2023-07-14 21:13:59 +0400

    lazy.nvim: add dressing

--- .config/lazyvim/lua/plugins/ui.lua
+++ .config/lazyvim/lua/plugins/ui.lua
@@ -76,4 +76,14 @@ return {
       vim.cmd [[colorscheme dracula]]
     end,
   },
+  {
+    'stevearc/dressing.nvim',
+    event = 'VeryLazy',
+    opts = {
+      input = {
+        insert_only = false,
+        start_in_insert = false,
+      },
+    },
+  },
 }
