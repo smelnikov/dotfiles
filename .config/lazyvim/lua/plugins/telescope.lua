6b00b030f85be5fe8ce70d114c350c2b587bff99
Sergei Melnikov <smelnikov@uniqsoft.ae>
2023-07-14 21:09:49 +0400

    lazy.nvim: add telescope

---
+++ .config/lazyvim/lua/plugins/telescope.lua
@@ -0,0 +1,43 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      cond = vim.fn.executable 'make' == 1,
+      build = 'make',
+    },
+  },
+  keys = {
+    {
+      desc = '[F]ind in [P]roject',
+      '<leader>fp',
+      ':Telescope find_files<CR>',
+      silent = true,
+    },
+    {
+      desc = '[L]ive [G]rep',
+      '<leader>lg',
+      ':Telescope live_grep<CR>',
+      silent = true,
+    },
+    {
+      desc = 'Git files',
+      '<C-p>',
+      ':Telescope git_files<CR>',
+      silent = true,
+    },
+
+    {
+      desc = '[B]uffers',
+      '<leader>b',
+      ':Telescope buffers<CR>',
+      silent = true,
+    },
+  },
+  config = function(_, opts)
+    require('telescope').setup(opts)
+    pcall(require('telescope').load_extension, 'fzf')
+  end,
+}
