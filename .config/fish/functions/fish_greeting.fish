b844ecad86b487ac6006d8992611b83ddff2827a
Sergei Melnikov <smelnikov@uniqsoft.ae>
2023-11-04 22:36:59 +0400

    fish: customize prompt

---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,7 @@
+function fish_greeting -d "What's up, fish?"
+    set_color $fish_color_autosuggestion
+    uname -nmsr
+    command -s uptime >/dev/null
+    and command uptime
+    set_color normal
+end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,16 +1,32 @@
 function fish_prompt
-    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
-        set -g VIRTUAL_ENV_DISABLE_PROMPT true
-    end
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
+  if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+    set -g VIRTUAL_ENV_DISABLE_PROMPT true
+  end
 
-    # Line 2
-    echo
-    if test -n "$VIRTUAL_ENV"
-        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-    end
-    printf '↪ '
-    set_color normal
+  set -l cwd (prompt_pwd)
+
+  set -l normal_color     (set_color normal)
+  set -l python_color     (set_color bryellow)
+  set -l node_color     (set_color brgreen)
+  set -l repo_color (set_color cyan)
+
+  if type -q nvm; and type -q node
+      set node_version (node -v 2>/dev/null) 
+      echo -n -s $node_color $node_version $normal_color " "
+  end
+
+  if test -n "$VIRTUAL_ENV"
+    set venv (basename $VIRTUAL_ENV)
+    echo -n -s $python_color $venv $normal_color " "
+  end
+
+  echo -n -s $normal_color $cwd $normal_color
+
+  if git_is_repo
+    echo -n -s " " $repo_color (git_branch_name) $normal_color " "
+  end
+
+  echo
+  # Line 2
+  echo -n -s "> "
 end
