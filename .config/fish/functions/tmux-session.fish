e3ee070a1dcd0981f8492e68f85ba579379ad555
Sergei Melnikov <smelnikov@uniqsoft.ae>
2023-11-16 23:46:44 +0400

    tmux-session: start a session in the specified dir

--- .config/fish/functions/tmux-session.fish
+++ .config/fish/functions/tmux-session.fish
@@ -1,9 +1,10 @@
-function tmux-session
-  set -l cwd (find ~/IdeaProjects -mindepth 2 -maxdepth 2 -type d | fzf)
+function tmux-session -a cwd
+  if test ! -n "$cwd"
+    set cwd (find ~/IdeaProjects -mindepth 2 -maxdepth 2 -type d | fzf)
+  end
   set -l name (basename "$cwd" | tr . _)
 
   tmux has-session -t=$name 2> /dev/null
-
   if test ! $status -eq 0
       tmux new-session -ds $name -c $cwd
   end
