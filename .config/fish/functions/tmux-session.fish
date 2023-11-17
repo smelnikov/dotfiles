19c6bbe77adb2b3de7fe0d90b3bca3880f21b408
Sergei Melnikov <smelnikov@uniqsoft.ae>
2023-11-17 14:45:09 +0400

    tmux-session: lower-case session name

--- .config/fish/functions/tmux-session.fish
+++ .config/fish/functions/tmux-session.fish
@@ -2,7 +2,7 @@ function tmux-session -a cwd
   if test ! -n "$cwd"
     set cwd (find ~/IdeaProjects -mindepth 2 -maxdepth 2 -type d | fzf)
   end
-  set -l name (basename "$cwd" | tr . _)
+  set -l name (basename "$cwd" | tr '[:upper:]' '[:lower:]')
 
   tmux has-session -t=$name 2> /dev/null
   if test ! $status -eq 0
