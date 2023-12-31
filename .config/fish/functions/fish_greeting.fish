ec3bc0fb00e05ec8a90d3095c57a8694263d88dd
Sergei Melnikov <m23nikov@gmail.com>
2024-01-01 03:26:38 +0400

    fish: remove greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,7 +1,2 @@
 function fish_greeting -d "What's up, fish?"
-    set_color $fish_color_autosuggestion
-    uname -nmsr
-    command -s uptime >/dev/null
-    and command uptime
-    set_color normal
 end
