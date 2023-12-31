cc5d633c6211e8a59fb799e1f9aa4b59ec82f6be
Sergei Melnikov <m23nikov@gmail.com>
2024-01-01 03:27:25 +0400

    fish: export variables from config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+set -gx VISUAL nvim
+set -gx EDITOR nvim
+set -gx MANPAGER nvim -c 'Man!' -o -
+set -gx FZF_DEFAULT_OPTS --color 16
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
