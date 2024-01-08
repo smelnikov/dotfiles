aa2eb743d65b61c748ebabab4c6f68078d6043bd
Sergei Melnikov <m23nikov@gmail.com>
2024-01-09 00:04:01 +0400

    fish: configure interactive mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,9 @@ set -gx VISUAL nvim
 set -gx EDITOR nvim
 set -gx MANPAGER nvim -c 'Man!' -o -
 set -gx FZF_DEFAULT_OPTS --color 16
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  fish_hybrid_key_bindings
+  fish_config theme choose Dracula
 end
 
