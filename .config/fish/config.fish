5e0a96e3d4f8367b65006e010a7635fc00baf25c
Sergei Melnikov <m23nikov@gmail.com>
2024-01-15 23:34:44 +0400

    fish: add asdf version manager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,9 @@ set -gx EDITOR nvim
 set -gx MANPAGER nvim -c 'Man!' -o -
 set -gx FZF_DEFAULT_OPTS --color 16
 
+source ~/.asdf/asdf.fish
+source ~/.asdf/completions/asdf.fish
+
 if status is-interactive
   fish_hybrid_key_bindings
   fish_config theme choose Dracula
