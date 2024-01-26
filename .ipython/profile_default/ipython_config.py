031a713a4597a99a59df70727ec5a1f8144561ef
Sergei Melnikov <m23nikov@gmail.com>
2024-01-27 02:21:41 +0400

    ipython: display completions in column

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -2,3 +2,4 @@ c = get_config()  # noqa
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.modal_cursor = False
+c.TerminalInteractiveShell.display_completions = "column"
