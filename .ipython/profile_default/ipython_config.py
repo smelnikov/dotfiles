6892433f499acfd479b9456b8ed09c779f23e063
Sergei Melnikov <m23nikov@gmail.com>
2024-01-28 23:27:28 +0400

    ipython: disable mode prompt

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,5 +1,6 @@
 c = get_config()  # noqa
 c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.prompt_includes_vi_mode = False
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.modal_cursor = False
 c.TerminalInteractiveShell.display_completions = "column"
