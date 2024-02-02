de1fdd87854fcad7d67fd7aa72097d78b2859089
Sergei Melnikov <m23nikov@gmail.com>
2024-02-02 21:45:21 +0400

    nvim: build treesitter with TSUpdateSync

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     main = 'nvim-treesitter.configs',
     event = { 'BufReadPre', 'BufNewFile' },
-    build = function()
-      local ts_update =
-        require('nvim-treesitter.install').update { with_sync = true }
-      ts_update()
-    end,
+    build = ':TSUpdateSync',
     opts = {
       auto_install = true,
       highlight = {
