d97adad70eb3a3a2d0be7ee5c976d59f3e374054
Sergei Melnikov <m23nikov@gmail.com>
2026-01-05 15:30:13 +0400

    nvim: remove args in tsc_makeprg

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -48,4 +48,4 @@ vim.diagnostic.config {
 vim.g.netrw_use_errorwindow = 0
 vim.g.netrw_bufsettings = 'noma nomod nu relativenumber nowrap ro nobl'
 
-vim.g.tsc_makeprg = 'npx tsc --noEmit'
+vim.g.tsc_makeprg = 'npx tsc'
