b8fe0defbcfe31cf2c4bda7ae5f2d43848ffca6b
Sergei Melnikov <m23nikov@gmail.com>
2026-04-24 01:06:13 +0400

    nvim: migrate to nvim-treesitter main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,44 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    main = 'nvim-treesitter.configs',
-    event = { 'BufReadPre', 'BufNewFile' },
-    build = ':TSUpdateSync',
-    opts = {
-      auto_install = true,
-      highlight = { enable = true },
-    },
+    build = ':TSUpdate',
+    opts = {},
+    config = function(_, opts)
+      local nvim_treesitter = require 'nvim-treesitter'
+      nvim_treesitter.setup(opts)
+
+      local langs = {}
+      vim.list_extend(langs, nvim_treesitter.get_available(1))
+      vim.list_extend(langs, nvim_treesitter.get_available(2))
+      vim.list.unique(langs)
+
+      local filetypes = {}
+      for _, language in ipairs(langs) do
+        vim.list_extend(
+          filetypes,
+          vim.treesitter.language.get_filetypes(language)
+        )
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetypes,
+        callback = function(event)
+          local lang = vim.treesitter.language.get_lang(event.match)
+
+          local function start()
+            vim.treesitter.start(event.buf)
+          end
+
+          if
+            vim.tbl_contains(nvim_treesitter.get_installed 'parsers', lang)
+          then
+            start()
+          else
+            nvim_treesitter.install(lang):await(start)
+          end
+        end,
+      })
+    end,
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
